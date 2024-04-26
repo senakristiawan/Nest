383d48476a9ce03c94d723f5917d3479a7ad9d9b
senakristiawan <senakristiawan1@gmail.com>
2024-04-26 09:59:13 +0700

    Initial Commit

---
+++ Nest/NestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestApp.swift
+//  Nest
+//
+//  Created by Sena Kristiawan on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
